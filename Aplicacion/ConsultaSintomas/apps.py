1f05c7695d52122a81c04b81bf9dca1cf3c9741c
RomelAnte <rsag20092002@gmail.com>
2024-11-03 22:09:47 -0500

    formuario consulta de sintomas

--- Aplicacion/ConsultaSintomas/apps.py
+++ Aplicacion/ConsultaSintomas/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ConsultasintomasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ConsultaSintomas'
+    name = 'Aplicacion.ConsultaSintomas'
--- sistemaSalud/settings.py
+++ sistemaSalud/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Aplicacion.ConsultaSintomas'
 ]
 
 MIDDLEWARE = [
@@ -73,12 +74,12 @@ WSGI_APPLICATION = 'sistemaSalud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
 
